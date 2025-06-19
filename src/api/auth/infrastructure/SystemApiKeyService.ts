8eb5e1530e1022cd8160e8c3ef31d656629db816
Luis <luis.espinoza@bizland.tech>
2025-06-18 21:43:46 -0300

    improve validation

--- src/api/auth/infrastructure/SystemApiKeyService.ts
+++ src/api/auth/infrastructure/SystemApiKeyService.ts
@@ -1,4 +1,5 @@
 
+import { Query } from "node-appwrite";
 import { appWriteDBs } from "../../../config/dbConnections";
 import { ISystemApiKeyActions } from "../model/actions/ISystemApiKeyActions";
 import { ISystemRoles } from "../model/entities/ISystemRoles";
@@ -8,15 +9,18 @@ export const createApiKeyService = (): ISystemApiKeyActions => {
     
     return {
         isValid: async (key: string): Promise<{isValid: boolean, role: ISystemRoles}> => {
-            console.log("🚀 ~ isValid: ~ key:", key)
             // Here you would implement the logic to check if the API key is valid.
             // This could involve checking against a database or an in-memory store.
             // For demonstration purposes, let's assume we have a hardcoded valid key.
            try {
-                const res = await appWriteDBs.listDocuments(process.env.DEV_APP_WRITE_DB_ID, process.env.DEV_APP_WRITE_API_KEY_COLLECTION_ID);
-                const validApiKeys = res.documents
-                //<{isValid: boolean, role: ISystemRoles}>
-                if (validApiKeys.find((validKey: { apiKey: string }) => validKey.apiKey === key)) {
+                const apiKeyIsValid = await appWriteDBs.listDocuments(
+                        process.env.DEV_APP_WRITE_DB_ID, 
+                        process.env.DEV_APP_WRITE_API_KEY_COLLECTION_ID,
+                        [
+                            Query.equal('apiKey', key)
+                        ]
+                    );
+                if (apiKeyIsValid.documents?.length > 0) {
                     return {
                         isValid: true,
                         role: ISystemRoles.SUPERADMIN
