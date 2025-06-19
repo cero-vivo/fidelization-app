f198b142fb34b053db45d9a0ec0fd220e9119e68
Luis <luis.espinoza@bizland.tech>
2025-06-18 21:39:54 -0300

    add appwrite db and api key server validation

--- src/api/auth/infrastructure/SystemApiKeyService.ts
+++ src/api/auth/infrastructure/SystemApiKeyService.ts
@@ -1,34 +1,32 @@
 
+import { appWriteDBs } from "../../../config/dbConnections";
 import { ISystemApiKeyActions } from "../model/actions/ISystemApiKeyActions";
 import { ISystemRoles } from "../model/entities/ISystemRoles";
 
 export const createApiKeyService = (): ISystemApiKeyActions => {
+
+    
     return {
         isValid: async (key: string): Promise<{isValid: boolean, role: ISystemRoles}> => {
+            console.log("🚀 ~ isValid: ~ key:", key)
             // Here you would implement the logic to check if the API key is valid.
             // This could involve checking against a database or an in-memory store.
             // For demonstration purposes, let's assume we have a hardcoded valid key.
            try {
-                let validApiKeys: string[] = [];
-
-                const result = await new Promise<{isValid: boolean, role: ISystemRoles}>((resolve) => {
-                    setTimeout(() => {
-                        validApiKeys = ["valid-api-key-123", "another-valid-key-456"];
-                        if (validApiKeys.includes(key)) {
-                            resolve({
-                                isValid: true,
-                                role: ISystemRoles.SUPERADMIN
-                            });
-                        } else {
-                            resolve({
-                                isValid: false,
-                                role: ISystemRoles.GUEST
-                            });
-                        }
-                    }, 2000);
-                });
-
-                return result;
+                const res = await appWriteDBs.listDocuments(process.env.DEV_APP_WRITE_DB_ID, process.env.DEV_APP_WRITE_API_KEY_COLLECTION_ID);
+                const validApiKeys = res.documents
+                //<{isValid: boolean, role: ISystemRoles}>
+                if (validApiKeys.find((validKey: { apiKey: string }) => validKey.apiKey === key)) {
+                    return {
+                        isValid: true,
+                        role: ISystemRoles.SUPERADMIN
+                    }
+                } else {
+                    return {
+                        isValid: false,
+                        role: ISystemRoles.GUEST
+                    };
+                }
 
            } catch (error) {
                 console.error("Error validating API key:", error);
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { swagger } from '@elysiajs/swagger'
 import { registerController } from "./modules/auth/infrastructure/register.controller";
 import { walletController } from "./modules/wallet/infrastructure/controller/WalletController";
 import { apiPath, PORT } from "./config";
-import { systemAuthController } from "./ApiGateway/auth/infrastructure/SystemAuthController";
+import { systemAuthController } from "./api/auth/infrastructure/SystemAuthController";
 import cookie from "@elysiajs/cookie";
 
 const app = new Elysia({ prefix: apiPath })
